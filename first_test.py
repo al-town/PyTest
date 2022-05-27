7f4484ac7610e290189e9c7933930d88c1e8c39c
Alexandra <101049354+al-town@users.noreply.github.com>
2022-05-27 16:48:12 +0300

    Программа для учебного задания 19.2.3

---
+++ first_test.py
@@ -0,0 +1,23 @@
+from app.Calculator import Calculator
+
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_multiply_calculate_failed(self):
+        assert self.calc.multiply(self, 2, 2) == 5
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(self, 53, 26) == 79
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(self, 102, 3) == 34
+
+    def test_subtraction_calculate_correctly(self):
+        assert self.calc.subtraction(self, 48, 32) == 16
+
+
